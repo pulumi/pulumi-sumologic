7859c913c5e4af89f7706506cd9cad940491e077
stack72 <public@paulstack.co.uk>
2021-05-21 00:28:05 +0100

    Linting the provider

--- provider/cmd/pulumi-resource-sumologic/main.go
+++ provider/cmd/pulumi-resource-sumologic/main.go
@@ -17,9 +17,9 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 	sumologic "github.com/pulumi/pulumi-sumologic/provider"
 	"github.com/pulumi/pulumi-sumologic/provider/pkg/version"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 )
 
 func main() {
--- provider/cmd/pulumi-tfgen-sumologic/main.go
+++ provider/cmd/pulumi-tfgen-sumologic/main.go
@@ -15,9 +15,9 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfgen"
 	sumologic "github.com/pulumi/pulumi-sumologic/provider"
 	"github.com/pulumi/pulumi-sumologic/provider/pkg/version"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfgen"
 )
 
 func main() {
